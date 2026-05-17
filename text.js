8a3e445b8e1dd4e7bcccfd3199df0a1f307037d8
fperlar817 <fperlar817@g.educaand.es>
2026-05-17 13:39:00 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-mp9o6xyj-tymstav7c">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+<title>7- Instrumentos de evaluación | Recibe lo que das.</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Bienvenidos y bienvenidas a nuestra aventura deportiva. Durante las próximas sesiones aprenderemos a lanzar, recibir y jugar en equipo mediante retos y juegos divertidos. Al final del proyecto participaremos en un gran «Torneo de Puntería» donde demostraremos todo lo aprendido.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-mp9o6xyj-tymstav7c" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">1-Presentación del proyecto.</a>
+</li>
+<li> <a href="../html/2--informacion-para-el-alumnoa-y-familias.html" class="no-ch">2- Información para el alumno/a y familias.</a>
+</li>
+<li> <a href="../html/3--producto-final.html" class="no-ch">3- Producto final.</a>
+</li>
+<li> <a href="../html/4--curacion-de-contenidos.html" class="no-ch">4- Curación de contenidos.</a>
+</li>
+<li> <a href="../html/5--linea-temporal.html" class="no-ch">5- Línea temporal.</a>
+</li>
+<li> <a href="../html/6--criterios-de-evaluacion.html" class="daddy">6- Criterios de evaluación.</a>
+<ul class="other-section">
+<li> <a href="../html/guia-didactica.html" class="no-ch">Guía didáctica.</a>
+</li>
+</ul>
+</li>
+<li class="active"> <a href="../html/7--instrumentos-de-evaluacion.html" class="active no-ch">7- Instrumentos de evaluación</a>
+</li>
+<li> <a href="../html/8--creditos-y-metadatos.html" class="no-ch">8- Créditos y metadatos.</a>
+</li>
+</ul>
+</nav><main id="page-mp9o6xyj-tymstav7c" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">Recibe lo que das.</p>
+<p class="package-subtitle">Educación Física, Primaria, lanzamientos, recepciones, torneo, DUA</p></div>
+<div class="page-header"><h1 class="page-title">7- Instrumentos de evaluación</h1></div>
+</header><div id="page-content-page-mp9o6xyj-tymstav7c" class="page-content">
+<article id="block-1778348933266-58ldkomzq" class="box">
+<header class="box-head no-icon">
+<h1 class="box-title">Texto</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778348933264-hcpzjw85i" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778348933264-hcpzjw85i&quot;}">
+<div class="exe-text-template">
+    <p>Algunos de <strong>los instrumentos de evaluación</strong> utilizados para nuestra situación de aprendizaje han sido los siguientes:</p>
+<p>1-<strong> Rúbrica.</strong><br />Este instrumento se utiliza para evaluar el desempeño técnico y actitudinal al terminar la Situación de aprendizaje.<br />● Evalúa cuatro dimensiones clave: el registro de resultados en el carnet, la ejecución del gesto de lanzamiento, el control y dirección del mismo, y la actitud y participación durante el trabajo.<br />● Obtengo los datos mediante observación directa del docente mientras el alumnado completa las estaciones. </p>
+<figure class="exe-figure position-center" style="width: 800px; text-align: center;"><img src="../content/resources/Rúbrica producto final torneo de puntería.png" width="800" height="566" alt="" />
+<figcaption class="figcaption"><a href="Elaboración propia generada con iDoceo" class="author" target="_blank" rel="noopener">Felipa Mª Pérez Lara</a>. <a href="Rúbrica lanzamientos y recepciones" class="title" target="_blank" rel="noopener"><em>Rúbrica lanzamientos y recepciones</em></a> <span class="license"><span class="sep">(</span><a href="http://creativecommons.org/licenses/" class="license" target="_blank" rel="noopener">CC BY-SA</a><span class="sep">)</span></span></figcaption>
+</figure>
+<p><strong>2- Ficha de Autoevaluación Visual</strong><br />Este instrumento busca que el alumnado reflexione sobre su propio proceso de aprendizajede forma sencilla.<br />● Evalúo tres ítems fundamentales: la precisión en el lanzamiento hacia el compañero, la técnica de recepción (mirando la pelota y usando ambas manos) y la actitud/respeto hacia las reglas y los demás.<br />● Obtengo la siguiente información: El alumnado colorea o marca una de las tres caras (Muy bien, Regular, Mal) según su propia percepción al finalizar la tarea.<br /></p>
+<figure class="exe-figure position-center" style="width: 900px; text-align: center;"><img src="../content/resources/Autoevaluación SdA Recibe lo que das..png" width="900" height="684" alt="" />
+<figcaption class="figcaption">Felipa Mª Pérez Lara. <a href="Autoevaluación visual." class="title" target="_blank" rel="noopener"><em>Autoevaluación visual.</em></a> <span class="license"><span class="sep">(</span><a href="http://creativecommons.org/licenses/" class="license" target="_blank" rel="noopener">CC BY-SA</a><span class="sep">)</span></span></figcaption>
+</figure>
+<p><br /></p>
+<p><br /></p>
+<p><br /></p>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/guia-didactica.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/8--creditos-y-metadatos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-nc-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" class="license">Creative Commons: Reconocimiento - no comercial - compartir igual 4.0</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
